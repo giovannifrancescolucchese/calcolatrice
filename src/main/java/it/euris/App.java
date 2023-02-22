59f2336be63b7a3a012cea7ecf7d990616441472
Giovanni F. Lucchese <info@lucchese.name>
2023-02-22 11:03:02 +0100

    tests in main

--- src/main/java/it/euris/App.java
+++ src/main/java/it/euris/App.java
@@ -10,11 +10,75 @@ public class App
     {
 
         Calcolatrice calcolatrice=new Calcolatrice();
-        int somma=calcolatrice.add(1,1);
+        /*TEST RELATIVO ALLA SOMMA*/
+        //TEST 1
+        Integer somma=calcolatrice.add(1,1);
         boolean testResult=somma==2;
         if (testResult)
             System.out.println("il risultato ottenuto è ugale a quello atteso ... OK");
         else
             System.out.println("il risultato ottenuto non rispecchia le aspettative ... KO");
+        //TEST 2
+        somma=calcolatrice.add(4,5);
+        testResult=somma!=2;
+        if (testResult)
+            System.out.println("il risultato ottenuto è ugale a quello atteso ... OK");
+        else
+            System.out.println("il risultato ottenuto non rispecchia le aspettative ... KO");
+
+        /*TEST RELATIVO ALLA SOTTRAZIONE*/
+        //TEST 1
+        Integer sottrazione=calcolatrice.sub(1,1);
+        testResult=sottrazione==0;
+        if (testResult)
+            System.out.println("il risultato ottenuto è ugale a quello atteso ... OK");
+        else
+            System.out.println("il risultato ottenuto non rispecchia le aspettative ... KO");
+        //TEST 2
+        sottrazione=calcolatrice.sub(4,5);
+        testResult=sottrazione!=2;
+        if (testResult)
+            System.out.println("il risultato ottenuto è ugale a quello atteso ... OK");
+        else
+            System.out.println("il risultato ottenuto non rispecchia le aspettative ... KO");
+
+        /*TEST RELATIVO ALLA MOLTIPLICAZIONE*/
+        //TEST 1
+        Integer moltiplicazione=calcolatrice.mul(1,1);
+        testResult=moltiplicazione==1;
+        if (testResult)
+            System.out.println("il risultato ottenuto è ugale a quello atteso ... OK");
+        else
+            System.out.println("il risultato ottenuto non rispecchia le aspettative ... KO");
+        //TEST 2
+        moltiplicazione=calcolatrice.mul(4,5);
+        testResult=moltiplicazione!=2;
+        if (testResult)
+            System.out.println("il risultato ottenuto è ugale a quello atteso ... OK");
+        else
+            System.out.println("il risultato ottenuto non rispecchia le aspettative ... KO");
+
+        /*TEST RELATIVO ALLA DIVISIONE */
+        //TEST 1
+        Integer divisione=calcolatrice.div(1,1);
+        testResult=divisione==1;
+        if (testResult)
+            System.out.println("il risultato ottenuto è ugale a quello atteso ... OK");
+        else
+            System.out.println("il risultato ottenuto non rispecchia le aspettative ... KO");
+        //TEST 2
+        divisione=calcolatrice.div(4,0);
+        if (divisione!=null)
+            testResult=divisione!=20;
+        else testResult=true;
+        if (testResult)
+            System.out.println("il risultato ottenuto è ugale a quello atteso ... OK");
+        else
+            System.out.println("il risultato ottenuto non rispecchia le aspettative ... KO");
+
+
     }
-}
+
+
+    }
+
--- src/main/java/it/euris/Calcolatrice.java
+++ src/main/java/it/euris/Calcolatrice.java
@@ -3,7 +3,29 @@ package it.euris;
 public class Calcolatrice {
     public Calcolatrice() {}
 
-    public int add(int a, int b) {
+    public Integer add(Integer a, Integer b) {
         return a+b;
     }
+
+    public Integer sub(Integer a, Integer b) {
+        return a-b;
+    }
+
+    public Integer mul(Integer a, Integer b) {
+        return a*b;
+    }
+
+    public Integer div(Integer a, Integer b) {
+        Integer result=null;
+        try {
+            result=a/b;
+        }
+        catch (ArithmeticException e) {
+            System.out.println("Catturata eccezione: "+e.getMessage());
+        }
+        return result;
+    }
+
+
+
 }
