4989f68159257c1d3d02295c3e92fc0c5dbc14e9
Giovanni F. Lucchese <info@lucchese.name>
2023-02-22 22:23:28 +0100

    best practices (beforeeach/beforeall) Given-When-Then

--- src/test/java/it/euris/CalcolatriceTest.java
+++ src/test/java/it/euris/CalcolatriceTest.java
@@ -1,63 +1,65 @@
 package it.euris;
 
 import it.euris.exceptions.CalculatorException;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.DisplayName;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 public class CalcolatriceTest {
+    static private Calcolatrice calcolatrice;
+
+
+    @BeforeAll
+    //@BeforeEach
+    static void initializeCalcolatrice() {
+        calcolatrice=new Calcolatrice();
+    }
 
     @Test
     @DisplayName("precondition=>a=1 AND b=1, sut=>Calcolatrice.add, postcondition=>a+b=2")
-    void addTwoNumbers() {
-        Calcolatrice calcolatrice=new Calcolatrice();
+    void givenAddWhenBoth1ThenReturn2() {
         assertEquals(2, calcolatrice.add(1,1),"1 +1 should be 2");
     }
 
     @Test
     @DisplayName("precondition=>a=4 AND b=5, sut=>Calcolatrice.add, postcondition=>a+b=9")
-    void addTwoNumbers_2() {
-        Calcolatrice calcolatrice=new Calcolatrice();
+    void givenAddWhenA4AndB5ThenReturn9() {
         assertEquals(9, calcolatrice.add(4,5),"4 +5 should be 9");
     }
 
     @Test
     @DisplayName("precondition=>a=4 AND b=0, sut=>Calcolatrice.div, postcondition=>Exception")
-    void divTwoNumbers() {
-        Calcolatrice calcolatrice=new Calcolatrice();
+    void givenDivWhenA4AndB0ThenCalculatorException() {
         assertThrows(CalculatorException.class, () -> calcolatrice.div(9,0));
     }
 
 
-
     @Test
     @DisplayName("precondition=>a=4 AND b=0, sut=>Calcolatrice.el, postcondition=>4^0=1")
-    void elTwoNumbers() {
-        Calcolatrice calcolatrice=new Calcolatrice();
+    void givenElWhenA4AndB0ThenReturn1() {
         assertEquals(1, calcolatrice.el(4,0),"4^0 should be 1");
     }
 
     @Test
     @DisplayName("precondition=>a=4 AND b=0, sut=>Calcolatrice.el, postcondition=>4^0!=0")
-    void elTwoNumbersNot() {
-        Calcolatrice calcolatrice=new Calcolatrice();
+    void givenElWhenA4AndB0ThenNotReturn0() {
         assertNotEquals(0, calcolatrice.el(4,0),"4^0 should not be 0");
     }
 
 
     @Test
     @DisplayName("precondition=>a=10 , sut=>Calcolatrice.sqrt, postcondition=>sqrt(10)==3")
-    void sqrtNumber() {
-        Calcolatrice calcolatrice=new Calcolatrice();
+    void givenSqrtWhenMin10ThenReturnCalculatorException() {
         assertThrows(CalculatorException.class,()->calcolatrice.sqrt(-10));
     }
 
     @Test
     @DisplayName("precondition=>a=10 , sut=>Calcolatrice.sqrt, postcondition=>sqrt(10)==3")
-    void sqrtNumber_2() throws CalculatorException {
-        Calcolatrice calcolatrice=new Calcolatrice();
-        assertEquals(3, calcolatrice.sqrt(-10),"sqrt 10 sould be 3");
+    void givenSqrtWhen10ThenReturn3() throws CalculatorException {
+        assertEquals(3, calcolatrice.sqrt(10),"sqrt 10 sould be 3");
     }
 
 }
